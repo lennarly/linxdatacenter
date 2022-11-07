9a664c4c4eb1d1eec50a38ab6454271b8015a73b
lennarly <yourlenar@gmail.com>
2022-11-07 22:59:19 +0500

    Refactor

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,12 +8,12 @@ import (
 func Run() {
 	file := product.New("internal/testdata/red.json")
 
-	products, err := file.Get()
+	p, err := file.Get()
 	if err != nil {
 		fmt.Println("An error occurred:", err)
 		return
 	}
 
-	fmt.Printf("The highest priced product: %v\n", products.GetHighPriced())
-	fmt.Printf("Top rated product: %v\n", products.GetHighRanked())
+	fmt.Printf("The highest priced product: %v\n", p.GetHighPriced())
+	fmt.Printf("Top rated product: %v\n", p.GetHighRanked())
 }
